f6e907e560583422d0ade72d7677d40aed2f7e78
yassine <ouaal.yassine@gmail.com>
2023-11-30 07:50:13 -0800

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import sys
+
+if __name__ == "__main__":
+    argv = sys.argv
+    argc = len(argv)
+
+    if argc == 1:
+        print("0 arguments.")
+    elif argc == 2:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(argc - 1))
+
+    for i in range (1, argc):
+        print("{}: {}".format(i, argv[i]))
