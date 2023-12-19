92146f08e612235a891d758c6bd7acefe61b5469
yassine <ouaal.yassine@gmail.com>
2023-12-19 14:18:14 -0800

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Module 0-square"""
+
+
+class Square:
+    """Square documentation"""
+
+    def __init__(self, size):
+        """Initializes the data."""
+        self.__size = size
