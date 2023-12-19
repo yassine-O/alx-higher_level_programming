256dfc475faeeb8923f8f8ec70f8a02e8ad81bfc
yassine <ouaal.yassine@gmail.com>
2023-12-19 14:30:08 -0800

    3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""Module 0-square"""
+
+
+class Square:
+    """Square documentation"""
+
+    def __init__(self, size=0):
+        """Initializes the data."""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """Instance method"""
+        return self.__size ** 2
