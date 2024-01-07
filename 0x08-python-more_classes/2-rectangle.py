f8c697ea6d96525bd71956b223d1ab7c4c87d769
yassine <ouaal.yassine@gmail.com>
2024-01-06 16:12:51 -0800

    2-rectangle.py

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+"""Module 0-rectangle"""
+
+
+class Rectangle:
+    """Rectangle documentation"""
+
+    def __init__(self, width=0, height=0):
+        """Initializes the data."""
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """ Getter returns width """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter method"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ Getter returns height """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter method"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Instance method"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Instance method"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__width + self.__height) * 2
