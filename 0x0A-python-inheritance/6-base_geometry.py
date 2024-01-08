772946b740fd21ca6dfc9efd16d0ae661e223711
yassine <ouaal.yassine@gmail.com>
2024-01-08 15:16:23 -0800

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Module 5-base_geometry.py"""
+
+
+class BaseGeometry:
+    """ BaseGeometry documentation """
+
+    def area(self):
+        """ area method """
+        raise Exception("area() is not implemented")
