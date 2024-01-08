8f5b4fa59f2b3ac65ceea7b46ea11524bf3d830c
yassine <ouaal.yassine@gmail.com>
2024-01-08 14:32:55 -0800

    1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
-"""Module 1-my_list.py"""
+""" Module 1-my_list.py """
 
 
 class MyList(list):
     """ MyList documentation """
-    
+
     def print_sorted(self):
         """ print_sorted doc """
         print(sorted(self))
