04eef9c663a15fe04335b739d1547ea5b7b2f70c
Yassine <yassine@gmail.com>
2024-03-13 10:42:51 -0700

    11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,6 +3,6 @@ if (process.argv.length <= 3) {
   console.log('0');
 } else {
   const array = process.argv.slice(2);
-  array.reverse();
+  array.sort((a, b) => b - a);
   console.log(array[1]);
 }
