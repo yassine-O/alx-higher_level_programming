32c3a439806b373b1e068312692ce1612c8f7418
Yassine <yassine@gmail.com>
2024-03-28 04:09:48 -0700

    0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Python script that fetches https://alx-intranet.hbtn.io/status"""
+
+import urllib.request
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+        print('Body response:')
+        print(f"\t- type: {type(html)}")
+        print(f"\t- content: {html}")
+        print(f"\t- utf8 content: " + html.decode('utf8'))
