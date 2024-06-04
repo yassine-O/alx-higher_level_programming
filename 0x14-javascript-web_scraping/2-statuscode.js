fda834de77107f2eb06716bce71345b6f82730ea
Yassine <yassine@gmail.com>
2024-06-04 04:05:07 -0700

    2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,5 +3,9 @@
 const url = process.argv[2];
 const request = require('request');
 request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+    return;
+  }
   console.log('code:', response.statusCode);
 });
