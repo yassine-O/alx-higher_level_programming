ddfe97b4b86c768f1d230ec9ac78dab982908598
Yassine <yassine@gmail.com>
2024-06-04 04:10:51 -0700

    3-starwars_title.js

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const id = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + id;
+const request = require('request');
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  body = JSON.parse(body);
+  console.log(body['title']);
+});
