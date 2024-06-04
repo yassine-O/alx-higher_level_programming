0ce40f10f5cb8ee513840e2a0c42e7385f99212d
Yassine <yassine@gmail.com>
2024-06-04 07:52:10 -0700

    101-starwars_characters.js

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const id = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + id;
+const request = require('request');
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  const movie = JSON.parse(body);
+  for (const i in movie.characters) {
+    request(movie.characters[i] , function (error, response, body) {
+      if (error) {
+        console.log(error);
+        return;
+      }
+      console.log(JSON.parse(body).name);
+    });
+  }
+});
